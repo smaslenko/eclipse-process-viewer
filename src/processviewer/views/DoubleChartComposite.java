e64fa2777b2eb27aa186f2e726df847adb9831d8
eugenii.samarskyi <eugenii.samarskyi@globallogic.com>
2015-03-25 10:18:10 +0200

    Improved ui, used memory in percentage

--- src/processviewer/views/DoubleChartComposite.java
+++ src/processviewer/views/DoubleChartComposite.java
@@ -1,6 +1,8 @@
 package processviewer.views;
 
 import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import javax.management.Attribute;
@@ -18,10 +20,15 @@ import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
 import org.swtchart.Chart;
+import org.swtchart.IAxis;
 import org.swtchart.IAxisTick;
 import org.swtchart.IGrid;
+import org.swtchart.ILegend;
 import org.swtchart.ILineSeries;
+import org.swtchart.ILineSeries.PlotSymbolType;
 import org.swtchart.ISeries.SeriesType;
+import org.swtchart.LineStyle;
+import org.swtchart.Range;
 import org.swtchart.internal.axis.AxisSet;
 
 import processviewer.views.ProcessView.ProcessInfo;
@@ -106,6 +113,39 @@ public class DoubleChartComposite extends Composite {
 												// 1 decimal point precision
 	}
 
+	// RESTRICTED method. Follow this link to solve trouble
+	// http://stackoverflow.com/questions/860187/access-restriction-on-class-due-to-restriction-on-required-library-rt-jar
+	public long getMaxRAMAmount() {
+		com.sun.management.OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) java.lang.management.ManagementFactory.getOperatingSystemMXBean();
+		long maxRam =  bean.getTotalPhysicalMemorySize();
+		return maxRam;
+	}
+	
+	public long getFreeRAMAmount() {
+		com.sun.management.OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) java.lang.management.ManagementFactory.getOperatingSystemMXBean();
+		long freeRam =  bean.getFreePhysicalMemorySize();
+		return freeRam;
+	}
+	
+	public double getUsagesMemoryInPercent() {
+		double mb = 1024*1024; 
+		
+		double memoryUsed = (getMaxRAMAmount() - getFreeRAMAmount()) / mb;
+		double maxRam = getMaxRAMAmount() / mb;
+		double result =  ( ( 100 * memoryUsed / maxRam  ) / 100 );
+		result = round(result, 2);
+		return result;
+	}
+	
+	public static double round(double value, int places) {
+	    if (places < 0) throw new IllegalArgumentException();
+
+	    long factor = (long) Math.pow(10, places);
+	    value = value * factor;
+	    long tmp = Math.round(value);
+	    return (double) tmp / factor;
+	}
+	
 	public Chart getMemoryChart() {
 		return memoryChart;
 	}
@@ -123,14 +163,23 @@ public class DoubleChartComposite extends Composite {
 	private void setupAxises(Chart chart) {
 
 		AxisSet set = (AxisSet) chart.getAxisSet();
-
+		
+		IAxis axisY = set.getYAxis(0);
+		if(chart.getTitle().getText().equals("Cpu Usage History")){
+			axisY.setRange(new Range(0, 100));
+		}else{
+			double mb = 1024 * 1024;
+			axisY.setRange(new Range(0, (getMaxRAMAmount() / mb) ));
+		}
+		
 		IAxisTick xTick = set.getXAxis(0).getTick();
 		xTick.setVisible(false);
 		xTick.setTickMarkStepHint(25);
 		
 		IAxisTick yTick = set.getYAxis(0).getTick();
-		yTick.setVisible(false);
-		yTick.setTickMarkStepHint(20);
+		yTick.setVisible(true);
+		yTick.setTickMarkStepHint(25);
+		yTick.setForeground(COLOR_GREEN);
 
 		IGrid xGrid = set.getXAxis(0).getGrid();
 		IGrid yGrid = set.getYAxis(0).getGrid();
@@ -141,6 +190,7 @@ public class DoubleChartComposite extends Composite {
 		set.getXAxis(0).getTitle().setVisible(false);
 		set.getYAxis(0).getTitle().setVisible(false);
 		
+//		chart.getLegend().setPosition(SWT.TOP);
 //		chart.getAxisSet().getXAxis(0).getTitle().setText("Time (10 seconds)");
 //		chart.getAxisSet().getYAxis(0).getTitle().setText("Percent");
 	}
@@ -155,9 +205,11 @@ public class DoubleChartComposite extends Composite {
 			memoryLoad += processInfo.memory;
 		}
 		
+		System.out.println("Memory in percent: "+ getUsagesMemoryInPercent() +" %");
+		
 		if (index < HISTORY_SIZE) {
 			cpuHistory[index] = getProcessCpuLoad();
-			memoryHistory[index] = memoryLoad;
+			memoryHistory[index] = getUsagesMemoryInPercent();
 			index++;
 
 		} else {
@@ -166,7 +218,7 @@ public class DoubleChartComposite extends Composite {
 				memoryHistory[i] = memoryHistory[i + 1];
 			}
 			cpuHistory[HISTORY_SIZE - 1] = getProcessCpuLoad();
-			memoryHistory[HISTORY_SIZE - 1] = memoryLoad;
+			memoryHistory[HISTORY_SIZE - 1] = getUsagesMemoryInPercent();
 		}
 
 		memoryLoad = 0;
@@ -185,9 +237,16 @@ public class DoubleChartComposite extends Composite {
 //		for (int i = 0; i < HISTORY_SIZE; i++) {
 //			System.out.println("cpu load = " + cpuHistory[i]);
 //		}
-
-		System.out.println();
-
+	
+		for (int i = 0; i < HISTORY_SIZE; i++) {
+			System.out.println("memory load = " + memoryHistory[i]);
+		}
+		
+		double mb = 1024 * 1024; 
+		double gb = 1024 * mb;
+		System.out.println("Total memory      : " + getMaxRAMAmount() / mb +" Mb");
+		System.out.println("Total memory Free : " + ( getFreeRAMAmount() / mb ) + " Mb");
+		System.out.println("Total memory Usage: " + ( (getMaxRAMAmount() - getFreeRAMAmount()) / mb ) + " Mb");
 		switch (type) {
 		case ALL:
 
@@ -226,18 +285,17 @@ public class DoubleChartComposite extends Composite {
 		resetSeries(cpuSeriesId, memorySeriesId);
 		setupLines(cpuSeriesId, memorySeriesId);
 
-		System.out.println("CPU Single process : "  + this.currentProcess.name);
-		System.out.println("-------------------------------------");
-		for (int i = 0; i < HISTORY_SIZE; i++) {
-			System.out.println("cpu load = " + cpuHistorySingle[i]);
-		}
-		
-		System.out.println("MEMORY Single process : "  + this.currentProcess.name);
-		
-		for (int i = 0; i < HISTORY_SIZE; i++) {
-			System.out.println("memory load = " + memoryHistorySingle[i]);
-		}
-		System.out.println("-------------------------------------");
+//		System.out.println("CPU Single process : "  + this.currentProcess.name);
+//		System.out.println("-------------------------------------");
+//		for (int i = 0; i < HISTORY_SIZE; i++) {
+//			System.out.println("cpu load = " + cpuHistorySingle[i]);
+//		}
+//		
+//		System.out.println("MEMORY Single process : "  + this.currentProcess.name);
+//		
+//		for (int i = 0; i < HISTORY_SIZE; i++) {
+//			System.out.println("memory load = " + memoryHistorySingle[i]);
+//		}
 	}
 	
 	public void resetProcess(){
@@ -260,9 +318,11 @@ public class DoubleChartComposite extends Composite {
 		
 		lineSeries1.setLineColor(Display.getDefault().getSystemColor(
 				SWT.COLOR_RED));
-		lineSeries1.enableArea(true);
+		lineSeries1.enableArea(false);
 		lineSeries1.getLabel().setVisible(true);
-
+		lineSeries1.setSymbolType(PlotSymbolType.NONE);
+		lineSeries1.setLineWidth(2);
+		
 		ILineSeries lineSeries2 = (ILineSeries) memoryChart.getSeriesSet()
 				.createSeries(SeriesType.LINE, memoryId);
 		if(type == ChartType.SINGLE){
@@ -273,9 +333,11 @@ public class DoubleChartComposite extends Composite {
 		
 		lineSeries2.setLineColor(Display.getDefault().getSystemColor(
 				SWT.COLOR_RED));
-		lineSeries2.enableArea(true);
+		lineSeries2.enableArea(false);
 		lineSeries2.getLabel().setVisible(true);
-
+		lineSeries2.setSymbolType(PlotSymbolType.NONE);
+		lineSeries2.setLineWidth(2);
+		
 		cpuChart.getAxisSet().adjustRange();
 		memoryChart.getAxisSet().adjustRange();
 	}
